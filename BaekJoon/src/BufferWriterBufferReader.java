dc14f48a8feb7bab66c8ca59ccf5921e429673e1
hyunboklee <gusqhrzkq@naver.com>
2022-04-29 13:57:51 +0900

    Fix : StringBuilder 구조

--- BaekJoon/src/BufferWriterBufferReader.java
+++ BaekJoon/src/BufferWriterBufferReader.java
@@ -4,17 +4,35 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class BufferWriterBufferReader {
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
-		String s = bf.readLine(); // String
-		int i = Integer.parseInt(bf.readLine()); // I
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		StringTokenizer st = null; // 공백을 기준으로 입력 받을 수 있게 해주는 모듈
+
+		String bfs = br.readLine(); // 문자열 (공백 미포함)
+		int bfn = Integer.parseInt(br.readLine()); // 문자열로 입력 받은 것을 숫자로 변환하여 저장 (공백 미포함)
+
+		st = new StringTokenizer(br.readLine()); // Ex) "1 2"이라고 입력하면 공백을 기준으로 1,2 형태로 저장됨 (defalt가 " "이다)
+		int stx = Integer.parseInt(st.nextToken()); // st.nextToken()을 출력한다면 st 안에 있는 순서대로 출력
+		int sty = Integer.parseInt(st.nextToken()); // st.nextToken()을 출력한다면 st 안에 있는 순서대로 출력
+
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 할당된 버퍼에 값 넣어주기
-		String t = "abcdefg"; // 출력할 문자열
-		bw.write(s + "\n" + t + "\n" + i); // 버퍼에 있는 값 전부 출력
+
+		StringBuilder sb = new StringBuilder(); // 할당된 버퍼에 값 넣어주기
+
+		// StringBuilder에 대한 출력문 저장
+		sb.append(bfs + " " + bfn + "\n");
+
+		// bufferedWriter에 대한 출력문 저장
+		bw.write(stx + " = " + sty);
+
+		System.out.print(sb);// StringBuilder에 있는 데이터 모두 출력
+
 		System.out.println("============================");
-		bw.flush(); // 남아있는 데이터를 모두 출력시킴
+
+		bw.flush(); // BufferedWriter에 남아있는 데이터 모두 출력
 		bw.close(); // 스트림을 닫음
 	}
 }
