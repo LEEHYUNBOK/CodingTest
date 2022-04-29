486f5b17e6734f0a7ae3143c7b480f6507f2a881
hyunboklee <gusqhrzkq@naver.com>
2022-04-29 14:40:15 +0900

    fix : StringTokenizer 수정

--- BaekJoon/src/BufferWriterBufferReader.java
+++ BaekJoon/src/BufferWriterBufferReader.java
@@ -9,12 +9,13 @@ import java.util.StringTokenizer;
 public class BufferWriterBufferReader {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
-		StringTokenizer st = null; // 공백을 기준으로 입력 받을 수 있게 해주는 모듈
 
 		String bfs = br.readLine(); // 문자열 (공백 미포함)
 		int bfn = Integer.parseInt(br.readLine()); // 문자열로 입력 받은 것을 숫자로 변환하여 저장 (공백 미포함)
 
-		st = new StringTokenizer(br.readLine()); // Ex) "1 2"이라고 입력하면 공백을 기준으로 1,2 형태로 저장됨 (defalt가 " "이다)
+		// 공백을 기준으로 입력 받을 수 있게 해주는 모듈
+		StringTokenizer st = new StringTokenizer(br.readLine()); // Ex) "1 2"이라고 입력하면 공백을 기준으로 1,2 형태로 저장됨 (defalt가 "
+																	// "이다)
 		int stx = Integer.parseInt(st.nextToken()); // st.nextToken()을 출력한다면 st 안에 있는 순서대로 출력
 		int sty = Integer.parseInt(st.nextToken()); // st.nextToken()을 출력한다면 st 안에 있는 순서대로 출력
 
