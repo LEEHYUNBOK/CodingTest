5086851740fd6ce57c38f8d6356bbdf40df7bd61
hyunboklee <gusqhrzkq@naver.com>
2022-05-03 13:42:30 +0900

    Feat : 단계  입출력과 사칙연산 추가

---
+++ BaekJoon/src/B25083.java
@@ -0,0 +1,8 @@
+
+public class B25083 {
+	// ¹®Á¦ : »õ½Ï
+	public static void main(String[] args) {
+		System.out.println(
+				"         ,r'\"7\n" + "r`-_   ,'  ,/\n" + " \\. \". L_r'\n" + "   `~\\/\n" + "      |\n" + "      |");
+	}
+}
